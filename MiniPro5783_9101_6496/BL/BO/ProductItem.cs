ba725fd146166239918aeeecf5b2b920a343eda7
michal <yisascha@g.jct.ac.il>
2023-01-10 15:30:59 +0200

    hhh

--- MiniPro5783_9101_6496/BL/BO/ProductItem.cs
+++ MiniPro5783_9101_6496/BL/BO/ProductItem.cs
@@ -5,7 +5,7 @@
         public int ID { get; set; }//product ID
         public string? Name { get; set; }
         public Category Category { get; set; }
-        public int InStock { get; set; }
+        public bool InStock { get; set; }
         public double Price { get; set; }
         public int Amount { get; set; }
 
